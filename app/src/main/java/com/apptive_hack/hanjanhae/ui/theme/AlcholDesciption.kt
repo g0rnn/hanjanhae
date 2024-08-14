92b1e821ad5403fcc5a5cc0afa609f67bafa24be
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-08-14 12:42:42 +0900

    feat: 상세 설명 화면 가려지지 않도록 개선

--- app/src/main/java/com/apptive_hack/hanjanhae/ui/theme/AlcholDesciption.kt
+++ app/src/main/java/com/apptive_hack/hanjanhae/ui/theme/AlcholDesciption.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -164,6 +165,7 @@ fun AlcholDescription(navController: NavController) {
                 .height(150.dp)
                 .align(Alignment.CenterHorizontally)
         )
+        Spacer(modifier = Modifier.height(200.dp))
     }
 }
 
